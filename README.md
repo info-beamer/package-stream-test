59a085199a662c2818d9aa8e54121bb5671f071b
Florian Wesch <fw@dividuum.de>
2018-03-12 17:19:28 +0100

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Import](https://cdn.infobeamer.com/s/img/import.png)](https://info-beamer.com/use?url=https://github.com/info-beamer/package-stream-test)
+
+# Description
+
+Uses the experimental streaming feature of info-beamer to show a video stream from a given url.
---
+++ node.lua
@@ -0,0 +1,50 @@
+gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
+
+util.noglobals()
+
+local video
+local url
+local next_try = 0
+
+local function stop_and_wait(t)
+    if video then
+        video:dispose()
+        video = nil
+    end
+    next_try = sys.now() + t
+end
+
+local function maybe_restart()
+    if url == "" then
+        return
+    end
+    if sys.now() < next_try then
+        return
+    end
+    video = resource.load_video{
+        file = url,
+        raw = true,
+    }
+end
+
+util.json_watch("config.json", function(config)
+    url = config.url
+    next_try = sys.now()
+    stop_and_wait(0)
+end)
+
+function node.render()
+    gl.clear(0, 0, 0, 1)
+
+    if video then
+        local state, w, h = video:state()
+        if state == "loaded" then
+            local x1, y1, x2, y2 = util.scale_into(NATIVE_WIDTH, NATIVE_HEIGHT, w, h)
+            video:target(x1, y1, x2, y2):layer(2)
+        elseif state == "finished" or state == "error" then
+            stop_and_wait(20)
+        end
+    else
+        maybe_restart()
+    end
+end
